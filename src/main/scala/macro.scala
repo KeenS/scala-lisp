0da77201b8c4710694a8eb2f2e762f5cec96e7cd
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-12-03 00:14:28 +0900

    call ToString

--- src/main/scala/macro.scala
+++ src/main/scala/macro.scala
@@ -5,12 +5,12 @@ import scala.language.experimental.macros
 
 object Macro {
   def lisp(expr: String): String = macro lisp_impl
-  def lisp_impl(c: Context)(expr: c.Expr[String]): c.Expr[String] = {
+  def lisp_impl(c: Context)(expr: c.Expr[String]): c.Tree = {
     import c.universe._
     val Literal(Constant(s_expr: String)) = expr.tree
     val ast = Parser.parseAll(Parser.expr, s_expr).get
     val t = toT(c)(ast)
-    c.Expr[String](q"""${t.toString}""")
+    q"Expr.ToString[$t]"
   }
 
   def toT(c: Context)(e: Ast.Ast): c.Tree = {
