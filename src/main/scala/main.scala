075fd5b881b140849a2af381a981d288d927faaf
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-12-03 13:06:33 +0900

    add some example in main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,12 @@
 package scalalisp
 
-import scala.language.higherKinds
-
-
-
+object Main {
+  def main(args: Array[String]) = {
+    println(Macro.lisp("1"))
+    println(Macro.lisp("(1 (nil) 2 3)"))
+    println(Macro.lisp("(+ 1 2)"))
+  }
+}
 
 
 
