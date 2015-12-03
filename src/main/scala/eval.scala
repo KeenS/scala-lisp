0ed6008a2abb72b0147c6132ebe8977670550645
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-12-03 20:08:34 +0900

    fix car, cdr and implement cons

--- src/main/scala/eval.scala
+++ src/main/scala/eval.scala
@@ -31,25 +31,34 @@ object Eval {
       type Out = E
     }
 
-  implicit def car[H <: Expr, T <: Expr, HOut <: Expr, TOut<: Expr, CarOut <: Expr]
+  implicit def car[E <: Expr, EOut <: Expr,  CarOut <: Expr]
     (implicit
-      h : Eval[H]{type Out = HOut},
-      t : Eval[T]{type Out = TOut},
-      car : Car[ConsCell[HOut, TOut]]{type Out = CarOut})
-      : Aux[ConsCell[Symbol[SCar], ConsCell[H, T]], CarOut] =
-    new Eval[ConsCell[Symbol[SCar], ConsCell[H, T]]] {
+      e : Eval[E]{type Out = EOut},
+      car : Car[EOut]{type Out = CarOut})
+      : Aux[ConsCell[Symbol[SCar], ConsCell[E, Nil]], CarOut] =
+    new Eval[ConsCell[Symbol[SCar], ConsCell[E, Nil]]] {
     type Out = CarOut
   }
 
-  implicit def cdr[H <: Expr, T <: Expr, HOut <: Expr, TOut<: Expr, CdrOut <: Expr]
+  implicit def cdr[E <: Expr, EOut <: Expr, CdrOut <: Expr]
     (implicit
-      h : Eval[H]{type Out = HOut},
-      t : Eval[T]{type Out = TOut},
-      cdr : Cdr[ConsCell[HOut, TOut]]{type Out = CdrOut})
-      : Aux[ConsCell[Symbol[SCdr], ConsCell[H, T]], CdrOut] =
-    new Eval[ConsCell[Symbol[SCdr], ConsCell[H, T]]] {
+      e : Eval[E]{type Out = EOut},
+      cdr : Cdr[EOut]{type Out = CdrOut})
+      : Aux[ConsCell[Symbol[SCdr], ConsCell[E, Nil]], CdrOut] =
+    new Eval[ConsCell[Symbol[SCdr], ConsCell[E, Nil]]] {
     type Out = CdrOut
   }
+
+  implicit def cons[L1 <: Expr, L2 <: Expr, L1Out <: Expr, L2Out<: Expr, ConsOut <: Expr]
+    (implicit
+      l1 : Eval[L1]{type Out = L1Out},
+      l2 : Eval[L2]{type Out = L2Out},
+      cons : Cons[L1Out, L2Out]{type Out = ConsOut})
+      : Aux[ConsCell[Symbol[SCons], ConsCell[L1, ConsCell[L2, Nil]]], ConsOut] =
+    new Eval[ConsCell[Symbol[SCons], ConsCell[L1, ConsCell[L2, Nil]]]] {
+    type Out = ConsOut
+  }
+
 
   implicit def append[L1 <: Expr, L2 <: Expr, L1Out <: Expr, L2Out<: Expr, AppendOut <: Expr]
     (implicit
@@ -60,6 +69,42 @@ object Eval {
     new Eval[ConsCell[Symbol[SAppend], ConsCell[L1, ConsCell[L2, Nil]]]] {
     type Out = AppendOut
   }
+
+
+  implicit def plus[L1 <: Expr, L2 <: Expr, L1Out <: Expr, L2Out<: Expr, PlusOut <: Expr]
+    (implicit
+      l1 : Eval[L1]{type Out = L1Out},
+      l2 : Eval[L2]{type Out = L2Out},
+      plus : Plus[L1Out, L2Out]{type Out = PlusOut})
+      : Aux[ConsCell[Symbol[SPlus], ConsCell[L1, ConsCell[L2, Nil]]], PlusOut] =
+    new Eval[ConsCell[Symbol[SPlus], ConsCell[L1, ConsCell[L2, Nil]]]] {
+    type Out = PlusOut
+  }
+
+
+  implicit def minus[L1 <: Expr, L2 <: Expr, L1Out <: Expr, L2Out<: Expr, MinusOut <: Expr]
+    (implicit
+      l1 : Eval[L1]{type Out = L1Out},
+      l2 : Eval[L2]{type Out = L2Out},
+      minus : Minus[L1Out, L2Out]{type Out = MinusOut})
+      : Aux[ConsCell[Symbol[SMinus], ConsCell[L1, ConsCell[L2, Nil]]], MinusOut] =
+    new Eval[ConsCell[Symbol[SMinus], ConsCell[L1, ConsCell[L2, Nil]]]] {
+    type Out = MinusOut
+  }
+
+
+  implicit def mult[L1 <: Expr, L2 <: Expr, L1Out <: Expr, L2Out<: Expr, MultOut <: Expr]
+    (implicit
+      l1 : Eval[L1]{type Out = L1Out},
+      l2 : Eval[L2]{type Out = L2Out},
+      mult : Mult[L1Out, L2Out]{type Out = MultOut})
+      : Aux[ConsCell[Symbol[SMult], ConsCell[L1, ConsCell[L2, Nil]]], MultOut] =
+    new Eval[ConsCell[Symbol[SMult], ConsCell[L1, ConsCell[L2, Nil]]]] {
+    type Out = MultOut
+  }
+
+
+
 
 
   trait EvalToString[E <: Expr] {
