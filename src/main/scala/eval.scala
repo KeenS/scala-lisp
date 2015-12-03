cb34cfa4fa5eae37ceba80337c7765a73ef83438
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-12-03 21:45:31 +0900

    eliminate `Aux`

--- src/main/scala/eval.scala
+++ src/main/scala/eval.scala
@@ -7,7 +7,6 @@ trait Eval[E <: Expr] {
 }
 
 trait EvalLowerPriority{ 
-  type Aux[E <:Expr, O <: Expr] = Eval[E]{type Out = O}
   type List1[E<:Expr] = ConsCell[E, Nil]
   type List2[E1<:Expr, E2<:Expr] = ConsCell[E1, List1[E2]]
   type List3[E1<:Expr, E2<:Expr, E3<:Expr] = ConsCell[E1, List2[E2, E3]]
@@ -16,62 +15,52 @@ trait EvalLowerPriority{
     (implicit
       cond : Eval[Cond]{type Out = CondOut},
       then_ : Eval[Then]{type Out = ThenOut})
-      : Aux[List4[Symbol[SIf], Cond, Then, Else], ThenOut] =
+      : Eval[List4[Symbol[SIf], Cond, Then, Else]] {type Out =  ThenOut} =
     new Eval[List4[Symbol[SIf], Cond, Then, Else]] {type Out =  ThenOut}
 
 }
 
 object Eval extends EvalLowerPriority {
-  def apply[E <: Expr](implicit eval: Eval[E]): Aux[E, eval.Out] = eval
+  def apply[E <: Expr](implicit eval: Eval[E]): Eval[E]{type Out = eval.Out} = eval
 
-  implicit def zero: Aux[Zero, Zero] = new Eval[Zero] {
-    type Out = Zero
-  }
-
-  implicit def succ[E <: Expr]: Aux[Succ[E], Succ[E]] = new Eval[Succ[E]] {
-    type Out = Succ[E]
-  }
-
-  implicit def nil: Aux[Nil, Nil] = new Eval[Nil] {
-    type Out = Nil
-  }
-  implicit def t: Aux[T_, T_] = new Eval[T_] {
-    type Out = T_
-  }
+  implicit def zero: Eval[Zero] {type Out = Zero} = new Eval[Zero] {type Out = Zero}
+  implicit def succ[E <: Expr]: Eval[Succ[E]] {type Out = Succ[E]} = new Eval[Succ[E]] {type Out = Succ[E]}
+  implicit def nil: Eval[Nil] {type Out = Nil} = new Eval[Nil] {type Out = Nil}
+  implicit def t: Eval[T_] {type Out = T_} = new Eval[T_] {type Out = T_}
 
   //TODO evaluate to bound value
-  implicit def symbol[S <: Sym]: Aux[Symbol[S], Symbol[S]] = new Eval[Symbol[S]] {type Out = Symbol[S]}
+  implicit def symbol[S <: Sym]: Eval[Symbol[S]] {type Out = Symbol[S]} = new Eval[Symbol[S]] {type Out = Symbol[S]}
 
-  implicit def quote[E <: Expr]: Aux[List2[Symbol[SQuote], E], E] =
+  implicit def quote[E <: Expr]: Eval[List2[Symbol[SQuote], E]] {type Out = E} =
     new Eval[List2[Symbol[SQuote], E]] {type Out = E}
 
   implicit def ifNil[Cond <: Expr, Then <: Expr, Else <: Expr, ElseOut <:Expr]
     (implicit
       cond : Eval[Cond]{type Out = Nil},
-      else_ : Eval[Else]{type Out = ElseOut})
-      : Aux[List4[Symbol[SIf], Cond, Then, Else], ElseOut] =
+      else_ : Eval[Else]{type Out = ElseOut}
+    ) : Eval[List4[Symbol[SIf], Cond, Then, Else]] {type Out = ElseOut} =
     new Eval[List4[Symbol[SIf], Cond, Then, Else]] {type Out = ElseOut}
 
   implicit def car[E <: Expr, EOut <: Expr,  CarOut <: Expr]
     (implicit
       e : Eval[E]{type Out = EOut},
-      car : Car[EOut]{type Out = CarOut})
-      : Aux[List2[Symbol[SCar], E], CarOut] =
+      car : Car[EOut]{type Out = CarOut}
+    ) : Eval[List2[Symbol[SCar], E]] {type Out = CarOut} =
     new Eval[List2[Symbol[SCar], E]] {type Out = CarOut}
 
   implicit def cdr[E <: Expr, EOut <: Expr, CdrOut <: Expr]
     (implicit
       e : Eval[E]{type Out = EOut},
-      cdr : Cdr[EOut]{type Out = CdrOut})
-      : Aux[List2[Symbol[SCdr],E], CdrOut] =
+      cdr : Cdr[EOut]{type Out = CdrOut}
+    ) : Eval[List2[Symbol[SCdr], E]] {type Out = CdrOut} =
     new Eval[List2[Symbol[SCdr], E]] {type Out = CdrOut}
 
   implicit def cons[L1 <: Expr, L2 <: Expr, L1Out <: Expr, L2Out<: Expr, ConsOut <: Expr]
     (implicit
       l1 : Eval[L1]{type Out = L1Out},
       l2 : Eval[L2]{type Out = L2Out},
-      cons : Cons[L1Out, L2Out]{type Out = ConsOut})
-      : Aux[List3[Symbol[SCons], L1, L2], ConsOut] =
+      cons : Cons[L1Out, L2Out]{type Out = ConsOut}
+    ) : Eval[List3[Symbol[SCons], L1, L2]] {type Out = ConsOut} =
     new Eval[List3[Symbol[SCons], L1, L2]] {type Out = ConsOut}
 
 
@@ -79,8 +68,8 @@ object Eval extends EvalLowerPriority {
     (implicit
       l1 : Eval[L1]{type Out = L1Out},
       l2 : Eval[L2]{type Out = L2Out},
-      append : Append[L1Out, L2Out]{type Out = AppendOut})
-      : Aux[List3[Symbol[SAppend], L1, L2], AppendOut] =
+      append : Append[L1Out, L2Out]{type Out = AppendOut}
+    ) : Eval[List3[Symbol[SAppend], L1, L2]] {type Out = AppendOut} =
     new Eval[List3[Symbol[SAppend], L1, L2]] {type Out = AppendOut}
 
 
@@ -88,8 +77,8 @@ object Eval extends EvalLowerPriority {
     (implicit
       l1 : Eval[L1]{type Out = L1Out},
       l2 : Eval[L2]{type Out = L2Out},
-      plus : Plus[L1Out, L2Out]{type Out = PlusOut})
-      : Aux[List3[Symbol[SPlus], L1, L2], PlusOut] =
+      plus : Plus[L1Out, L2Out]{type Out = PlusOut}
+    ) : Eval[List3[Symbol[SPlus], L1, L2]] {type Out = PlusOut} =
     new Eval[List3[Symbol[SPlus], L1, L2]] {type Out = PlusOut}
 
 
@@ -97,8 +86,8 @@ object Eval extends EvalLowerPriority {
     (implicit
       l1 : Eval[L1]{type Out = L1Out},
       l2 : Eval[L2]{type Out = L2Out},
-      minus : Minus[L1Out, L2Out]{type Out = MinusOut})
-      : Aux[List3[Symbol[SMinus], L1, L2], MinusOut] =
+      minus : Minus[L1Out, L2Out]{type Out = MinusOut}
+    ) : Eval[List3[Symbol[SMinus], L1, L2]] {type Out = MinusOut} =
     new Eval[List3[Symbol[SMinus], L1, L2]] {type Out = MinusOut}
 
 
@@ -106,8 +95,8 @@ object Eval extends EvalLowerPriority {
     (implicit
       l1 : Eval[L1]{type Out = L1Out},
       l2 : Eval[L2]{type Out = L2Out},
-      mult : Mult[L1Out, L2Out]{type Out = MultOut})
-      : Aux[List3[Symbol[SMult], L1, L2], MultOut] =
+      mult : Mult[L1Out, L2Out]{type Out = MultOut}
+    ) : Eval[List3[Symbol[SMult], L1, L2]] {type Out = MultOut} =
     new Eval[List3[Symbol[SMult], L1, L2]] {type Out = MultOut}
 
 
@@ -123,7 +112,8 @@ object Eval extends EvalLowerPriority {
   implicit def evalToString[E <:Expr, EvalOut <: Expr](
     implicit
       eval : Eval[E]{ type Out = EvalOut},
-    toString_ : Expr.ToString[EvalOut]): EvalToString[E] = new EvalToString[E]{def apply() = toString_()}
+    toString_ : Expr.ToString[EvalOut]
+  ): EvalToString[E] = new EvalToString[E]{def apply() = toString_()}
 }
 
 
--- src/main/scala/list.scala
+++ src/main/scala/list.scala
@@ -3,41 +3,41 @@ package scalalisp
 trait Car[C <: Expr] {type Out <: Expr}
 
 object Car {
-  def apply[C <: Expr](implicit car: Car[C]): Aux[C, car.Out] = car
-  type Aux[C <: Expr, O <: Expr] = Car[C]{type Out = O}
-  implicit def car[H <: Expr, T <: Expr]: Aux[ConsCell[H, T], H] = new Car[ConsCell[H, T]] {type Out = H}
+  def apply[C <: Expr](implicit car: Car[C]): Car[C] {type Out = car.Out} = car
+
+  implicit def car[H <: Expr, T <: Expr]: Car[ConsCell[H, T]] {type Out = H} =
+    new Car[ConsCell[H, T]] {type Out = H}
 }
 
 trait Cdr[C <: Expr] {type Out <: Expr}
 
 object Cdr {
-  def apply[C <: Expr](implicit cdr: Cdr[C]): Aux[C, cdr.Out] = cdr
-  type Aux[C <: Expr, O <: Expr] = Cdr[C]{type Out = O}
-  implicit def cdr[H <: Expr, T <: Expr]: Aux[ConsCell[H, T], T] = new Cdr[ConsCell[H, T]] {type Out = T}
+  def apply[C <: Expr](implicit cdr: Cdr[C]): Cdr[C] {type Out = cdr.Out} = cdr
+
+  implicit def cdr[H <: Expr, T <: Expr]: Cdr[ConsCell[H, T]] {type Out = T} =
+    new Cdr[ConsCell[H, T]] {type Out = T}
 }
 
 trait Cons[Car <: Expr, Cdr <: Expr] {type Out <: Expr}
 
 object Cons {
-  def apply[Car <: Expr, Cdr <: Expr](implicit cons : Cons[Car, Cdr]): Aux[Car, Cdr, cons.Out] = cons
-  type Aux[Car<: Expr, Cdr <: Expr, O] = Cons[Car, Cdr]{type Out = O}
-  implicit def cons[Car <: Expr, Cdr <: Expr]: Aux[Car, Cdr, ConsCell[Car,Cdr]] = new Cons[Car, Cdr]{type Out = ConsCell[Car, Cdr]}
+  def apply[Car <: Expr, Cdr <: Expr](implicit cons : Cons[Car, Cdr]): Cons[Car, Cdr]{type Out = cons.Out}  = cons
+
+  implicit def cons[Car <: Expr, Cdr <: Expr]: Cons[Car, Cdr]{type Out = ConsCell[Car, Cdr]} =
+    new Cons[Car, Cdr]{type Out = ConsCell[Car, Cdr]}
 }
 
 
 trait Append[L1 <: Expr, L2 <: Expr]{type Out <: Expr}
 
 object Append {
-  type Aux[L1 <: Expr, L2 <: Expr, O <: Expr] = Append[L1, L2]{type Out = O}
-  def apply[L1 <: Expr, L2 <: Expr](implicit append: Append[L1, L2]): Aux[L1, L2, append.Out] = append
+  def apply[L1 <: Expr, L2 <: Expr](implicit append: Append[L1, L2]):Append[L1, L2] {type Out = append.Out}  = append
 
 
-  implicit def appendNil[L1 <: Nil, L2 <: Expr]: Aux[L1, L2, L2] = new Append[L1, L2] {
-    type Out = L2
-  }
+  implicit def appendNil[L1 <: Nil, L2 <: Expr]: Append[L1, L2] {type Out = L2} = new Append[L1, L2] {type Out = L2}
 
   implicit def appendList[L1Car <: Expr, L1Cdr <: Expr, L2 <: Expr]
-    (implicit append_ : Append[L1Cdr, L2]): Aux[ConsCell[L1Car, L1Cdr], L2, ConsCell[L1Car, append_.Out]] =
+    (implicit append_ : Append[L1Cdr, L2]): Append[ConsCell[L1Car, L1Cdr], L2] {type Out = ConsCell[L1Car, append_.Out]} =
     new Append[ConsCell[L1Car, L1Cdr], L2] {type Out = ConsCell[L1Car, append_.Out]}
 
 }
